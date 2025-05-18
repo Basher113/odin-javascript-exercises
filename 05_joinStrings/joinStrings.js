db80d9b41ce162b7e449083b0077baa2a9ae70b7
basher113 <147272612+Basher113@users.noreply.github.com>
2025-05-19 07:04:15 +0800

    Finish exercises 1,2,3,4, and 5

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -14,6 +14,13 @@
 
 
 // Do not change this
+const firstName = "Carlos"
+const lastName = "Stevenson"
+const thisYear = 1965
+const birthYear = 1947
+const fullName = firstName + " " + lastName
+const age = thisYear - birthYear
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old."
 module.exports = {
   firstName: typeof firstName === 'undefined' ? undefined : firstName,
   lastName: typeof lastName === 'undefined' ? undefined : lastName,

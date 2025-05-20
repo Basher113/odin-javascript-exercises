0881e0446b5f6fe380919ff94c1adaddd885d475
basher113 <147272612+Basher113@users.noreply.github.com>
2025-05-21 06:27:06 +0800

    Finish exercise 08
    - Use array method filter to filter the array
    - Use include method to check if each item of array is in arguments

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    return arr.filter((item) => {
+        return !args.includes(item);
+    })
 };
+
+removeFromArray([1, 2, 3, 5, 2], 2)
 
 // Do not edit below this line
 module.exports = removeFromArray;

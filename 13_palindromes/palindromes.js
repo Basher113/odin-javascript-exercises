82305efe0fd352fdf4ef4f40d5e046a7812ac5f7
Basher113 <147272612+Basher113@users.noreply.github.com>
2025-05-23 11:30:40 +0800

    Finish exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const alphanumeric = removeNonAlphaNumeric(str);
+    let start = 0;
+    let end = alphanumeric.length - 1;
+    while (start <= end) {
+        const char1 = alphanumeric[start];
+        const char2 = alphanumeric[end];
+        if (char1 !== char2) return false;
+        start++;
+        end--;
+    }
+    return true;
 };
 
+const removeNonAlphaNumeric = (str) => {
+    const alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789"
+    return str.toLowerCase().split("").filter((char) => alphanumeric.includes(char)).join("");
+}
+palindromes("rac3e3car")
 // Do not edit below this line
 module.exports = palindromes;

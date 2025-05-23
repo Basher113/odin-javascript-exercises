1a418d1f8a62e127b6d9ee9692586fcd4a15edcc
Basher113 <147272612+Basher113@users.noreply.github.com>
2025-05-23 11:54:13 +0800

    Finish exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n < 0) return "OOPS" 
+    if (n === 0) return 0;
 
+    let start = 0;
+    let second = 1;
+    
+    for (let i = 1; i < n; i++) {
+        const next = start + second;
+        start = second;
+        second = next;
+    }
+    return second;
 };
+
+fibonacci("4")
 
 // Do not edit below this line
 module.exports = fibonacci;
